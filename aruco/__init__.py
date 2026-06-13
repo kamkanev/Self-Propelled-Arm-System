223a33f0fe9095a10122f20c0446937e643ff12b
kamkanev <kamkanev@gmail.com>
2026-06-13 19:57:09 +0200

    added quick access to aruco_dictionary and funtions

---
+++ aruco/__init__.py
@@ -0,0 +1,8 @@
+from .aruco_dict import ARUCO_DICT, get_default_tag_size, get_predefined_dictionary, get_max_marker_id
+
+__all__ = [
+    "ARUCO_DICT",
+    "get_default_tag_size",
+    "get_predefined_dictionary",
+    "get_max_marker_id",
+]
--- aruco/gui_generator.py
+++ aruco/gui_generator.py
@@ -3,30 +3,7 @@ import sys
 import numpy as np
 import cv2
 import pygame
-
-ARUCO_DICT = {
-    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11,
-}
+from aruco_dict import ARUCO_DICT, get_default_tag_size, get_max_marker_id
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 620
@@ -40,21 +17,6 @@ QUIT_BUTTON_HOVER = (240, 90, 90)
 TEXT_COLOR = (240, 240, 240)
 BG_COLOR = (30, 30, 30)
 BORDER_COLOR = (220, 220, 220)
-
-
-def get_default_tag_size(aruco_type):
-    parts = aruco_type.split("_")
-    for part in reversed(parts):
-        if part.isdigit():
-            return int(part)
-    return 250
-
-
-def get_max_marker_id(aruco_type):
-    dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
-    if hasattr(dictionary, "bytesList"):
-        return dictionary.bytesList.shape[0] - 1
-    return 999
 
 
 def generate_marker(aruco_type, marker_id, tag_size):
